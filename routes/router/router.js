db89327ff60624356eaf68cf8a4d957e5698bcd0
Artpietah <artpietah@gmail.om>
2025-07-21 14:10:56 +0300

    Update router

--- routes/router/router.js
+++ routes/router/router.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { createRouter, getAllRouters,getRouter,deleteRouter } = require('../../controllers/router/routerController');
+const { createRouter, getAllRouters,getRouter,deleteRouter, updateRouter } = require('../../controllers/router/routerController');
 const { verifyToken } = require('../../middleware/authMiddleware');
 const {isRouterOwner} = require('../../middleware/routerMiddleware');
 
-router.get('/', getAllRouters).post('/create', verifyToken, createRouter);
+router.get('/', getAllRouters).post('/create', verifyToken, createRouter)
 router.get('/:id', verifyToken, getRouter);
 router.delete('/:id', verifyToken, isRouterOwner,deleteRouter);
+router.put('/:id', verifyToken, updateRouter);
 
 
 module.exports = router;
