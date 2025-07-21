19e39ca16fbe2d58596a308b4ac6f9e165a3469f
Artpietah <artpietah@gmail.om>
2025-07-21 14:04:58 +0300

    Delete router

--- middleware/routerMiddleware.js
+++ middleware/routerMiddleware.js
@@ -31,5 +31,21 @@ const checkRouter = async (req, res, next) => {
     }
 }
 
+const isRouterOwner = async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const router = await Router.findById(id);
+        if (!router) {
+            return res.status(404).json({ message: 'Router not found' });
+        }
+        if (router.createdBy.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: 'You are not the owner of this router' });
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({ message: 'Error checking router ownership', error });
+    }
+};
 
-module.exports = { pingRouter, checkRouter};
+
+module.exports = { pingRouter, checkRouter, isRouterOwner };
--- routes/router/router.js
+++ routes/router/router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const { createRouter, getAllRouters,getRouter } = require('../../controllers/router/routerController');
+const { createRouter, getAllRouters,getRouter,deleteRouter } = require('../../controllers/router/routerController');
 const { verifyToken } = require('../../middleware/authMiddleware');
+const {isRouterOwner} = require('../../middleware/routerMiddleware');
 
 router.get('/', getAllRouters).post('/create', verifyToken, createRouter);
 router.get('/:id', verifyToken, getRouter);
+router.delete('/:id', verifyToken, isRouterOwner,deleteRouter);
 
 
 module.exports = router;
